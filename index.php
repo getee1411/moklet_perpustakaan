9da371ff919e7171e92decb16b9d676b09576fbb
getee1411 <62593648+getee1411@users.noreply.github.com>
2020-03-24 16:25:16 +0700

    Create index.php
    Menu awal login

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="box">
+        <h1>PERPUSTAKAAN MOKLET</h1>
+        <div class="ibox">
+            <h2>Masuk Sebagai</h2>
+            <a href="login_a.php" class="btn">ANGGOTA</a>
+            <a href="login_p.php" class="btn">PETUGAS</a>
+        </div>
+    </div>
+</body>
+</html>
